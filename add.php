97def2d07260f31acfe096363d3cfd0c77a3ff4c
thanosbhai <avakashverma98@gmail.com>
2019-02-24 00:21:15 +0530

    styling changes

--- add.php
+++ add.php
@@ -6,19 +6,25 @@
 
 	<link rel="stylesheet" type="text/css" href="Addstyle.css">
 </head>
-<body style="background-color: #E6E6FA">
+<body style="background-color:#110D21">
 	<form method="POST" action="Add.php">
 		
 		<div align="center" 
-		style="background-color: 	#ADD8E6 ;width:650px; margin-left:23%;
-		margin-right: 22%;height: 450px ;border-radius: 10px; padding-top:1px; margin-top:3%;">
-      <h1 align="center" style="font-family: garamond; color: #2F4F4F">RTI</h1>
+		style="background-color:#110D21 ;width:650px; margin-left:23%;
+		margin-right: 22%;height: 450px ;border-radius:10px; padding-top:1px; margin-top:3%;">
+      <h1 align="center" style="font-family: garamond; color:#D0C8F2;">RTI</h1>
 	 <table align="center" class="table" width="400" height="200">
 			<tr>
 				<th></th><th></th>
 			</tr>
 		    <tr>
-				<td align="center" colspan="2"><h3><font style="font-size: 1.2em; font-family: garamond; color: #2F4F4F">Query Submition</font></h3></td>
+				<td align="center" colspan="2">
+					<h3>
+						<font style="font-size: 1.2em; font-family: garamond; color:#E4E2EE">
+						Query Submition 
+						</font>
+					</h3>
+				</td>
 			</tr>
 			<tr>
 				<td style="font-family: bookman;font-size: 1.1em">Department</td>
