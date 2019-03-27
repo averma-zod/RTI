319534e4f31e42100344ac8aca08d278e720f581
thanosbhai <avakashverma98@gmail.com>
2019-03-27 15:51:36 +0530

    viewall

--- viewall.php
+++ viewall.php
@@ -3,16 +3,36 @@
 <html>
 <head>
 	<title>viewall</title>
+  <link rel="stylesheet" type="text/css" href="joinasstyle.css">
 	<style>
       table,th,tr,td{
           border:1px solid #483D8B;
           border-collapse: collapse;
-          padding:5px 100px 1px 10px;
+          padding:10px;
   </style>
 </head>
 
 
 <body style="background-color: white">
+
+  <header>
+    <img align="top" style=" border-radius: 50%; margin-left:5px;margin-top:5px; width: 70px;height: 70px" src="RTI.png">
+    <font  style=" font-size:4.5em; margin-left: 10px; font-family: garamond; color:white">RTI</font>
+    <div class="right-nav"><button class="navbtn">About</button><button class="navbtn">FAQ</button><button class="navbtn">Contact</button></div>    
+    </header>
+    
+        <div class="navigation">
+      <select class="navibtn" name="Filter">
+        <option hidden="">Queries</option>
+        <option value="Medical">Medical</option>
+        <option value="Education">Education</option>
+        <option value="Traffic">Traffic</option>
+      </select><br>
+      <button class="navibtn">Account Settings</button><br>
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <button class="logbtn" name="Logout">Logout</button>
+    </div>
+
 	<?php
      session_start();
      $db = mysqli_connect('localhost','root','','rti');
@@ -32,7 +52,7 @@
 
     <form method="POST" action="viewall.php">
 
-    <table align="left" style=" margin-left: 10px; margin-top: 10px">
+    <table align="left" style="width:86%; margin-left: 10px; margin-top: 10px">
         <thead>
             <th>Query</th>
             <th>Department</th>
@@ -66,16 +86,16 @@
                   <td><?php echo $Date; ?></td>
                   <td><?php echo $Status;?></td>
                   <td><?php echo $Answer; ?></td>
-                  <td ><?php
+                  <td align="center" ><?php
                     if($type == 'Admin')
                     {
                  	   if($Status == 'Unsolved')
                  	   {
-                 		 ?><button value="<?=$id ?>" name="Usolve">Reply</button><?php
+                 		 ?><button value="<?=$id ?>" class="reply" name="Usolve">Reply</button><?php
                  	   }
                  	   elseif ($Status == 'Solved') 
                  	   {
-                 		 ?><button value="<?=$id ?>" name="Solve">Change</button><?php
+                 		 ?><button value="<?=$id ?>" class="change" name="Solve">Change</button><?php
                  	   }
                     }
                   ?></td>
