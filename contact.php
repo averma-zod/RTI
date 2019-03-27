c1a735b2da2a2355f0894ad3e3634504d4332853
luckyat7 <36253810+luckyat7@users.noreply.github.com>
2019-03-27 23:31:30 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Contact Us</title>
+	<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700">
+	<link rel="stylesheet" type="text/css" href="contactstyle.css">
+</head>
+<body>
+		<div class="wrapper">
+			<div class="nav">
+				<div class="left">
+					<ul>
+						<li><a href="#">Business</a></li>
+						<li><a href="#">Jobs</a></li>
+						<li><a href="#">Counselling</a></li>
+					</ul>
+				</div>
+				<div class="right">
+					<ul>
+						<li><a href="#">Log In</a></li>
+						<li><a href="#"> Sign In</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="header">
+				<h1>Contact Us</h1>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+			</div>
+			<div class="form">
+				<form>
+					<input type="text" id="name" placeholder="Name" />
+					<input type="text" id="mail" placeholder="E-mail" />
+					<input type="text" id="msg" placeholder="Message" />
+				</form>
+				<button type="button">Let's Talk</button>
+			</div>
+		</div>
+
+</body>
+</html>
